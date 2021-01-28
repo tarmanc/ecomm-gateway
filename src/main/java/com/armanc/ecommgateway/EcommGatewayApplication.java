716f0a6cda6b1112ae3219fa777610296b234fb8
adyingwish <62962574+adyingwish@users.noreply.github.com>
2021-01-28 21:57:52 +0300

    initial add of project

---
+++ src/main/java/com/armanc/ecommgateway/EcommGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.armanc.ecommgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/armanc/ecommgateway/EcommGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.armanc.ecommgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
