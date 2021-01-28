8e82163c9742e525c50db4598116138d735edb7a
adyingwish <62962574+adyingwish@users.noreply.github.com>
2021-01-28 22:48:13 +0300

    Closes #1

---
+++ src/main/java/com/armanc/ecommgateway/config/GoogleConfig.java
@@ -0,0 +1,26 @@
+package com.armanc.ecommgateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("google")
+@Configuration
+public class GoogleConfig {
+
+    @Bean
+    public RouteLocator googleRouteConfig(RouteLocatorBuilder builder) {
+
+        return builder.routes()
+                .route("id",p -> p
+                        .path("/googlesearch")
+                        .filters(f -> f.rewritePath("/googlesearch(?<segment>/?.*)", "/${segment}"))
+                        .uri("https://google.com")
+                )
+                .build();
+    }
+
+
+}
